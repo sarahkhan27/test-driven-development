6d82609db3c8b8fab6526b1c73dbf63f6d6df7f2
khans5 <sk@class-docker>
2025-02-25 03:48:37 +0000

    adding condition

--- src/Experiment.py
+++ src/Experiment.py
@@ -3,3 +3,12 @@ class Experiment:
         """Initialize an empty experiment with a list to store SDT objects and labels."""
         self.conditions = []
         self.labels = []
+
+from src.SignalDetection import SignalDetection
+
+def add_condition(self, sdt_obj, label=None):
+    """Adds a SignalDetection object and an optional label to the experiment."""
+    if not isinstance(sdt_obj, SignalDetection):
+        raise TypeError("sdt_obj must be an instance of SignalDetection")
+    self.conditions.append(sdt_obj)
+    self.labels.append(label)
