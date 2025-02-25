9d7c0ac627230053dfc8e8dad8c812ede4a7944b
khans5 <sk@class-docker>
2025-02-25 03:23:41 +0000

    made changes within Experiment.py

---
+++ src/Experiment.py
@@ -0,0 +1,5 @@
+class Experiment:
+    def __init__(self):
+        """Initialize an empty experiment with a list to store SDT objects and labels."""
+        self.conditions = []
+        self.labels = []
